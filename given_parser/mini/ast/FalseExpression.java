e316e0c6852e071f5b28b43f5e91a807131c15c5
bbeck13 <bbeck13@ischool.org>
2017-04-10 21:34:22 -0700

    added value to false

--- given_parser/mini/ast/FalseExpression.java
+++ given_parser/mini/ast/FalseExpression.java
@@ -1,5 +1,10 @@
 package mini.ast;
 
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+@EqualsAndHashCode(callSuper=false)
+@Value
 public class FalseExpression extends AbstractExpression {
    public FalseExpression(int lineNum) {
       super(lineNum);
